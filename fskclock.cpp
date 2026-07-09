375d8584b6d27f8d7a2a8f0683cf687f575c735b
Jacob Vosmaer <contact@jacobvosmaer.nl>
2026-07-09 11:20:51 +0200

    Use analog knobs to remember BPM

--- fskclock.cpp
+++ fskclock.cpp
@@ -3,36 +3,29 @@
 #define assert(x)                                                              \
   if (!(x))                                                                    \
   asm("bkpt 255")
-#define min(x, y) ((x) < (y) ? (x) : (y))
-#define max(x, y) ((x) > (y) ? (x) : (y))
 using namespace daisy;
 DaisyPod hw;
-float phase, freq;
-int bpm = 120, ticks, tickreset;
-#define freqhi 2100
-#define freqlo 1300
+float phase;
+int ticks;
 float hztofreq(float hz) { return hz / hw.AudioSampleRate(); }
 static void audio(AudioHandle::InterleavingInputBuffer in,
                   AudioHandle::InterleavingOutputBuffer out, size_t size) {
   hw.ProcessAllControls();
-  bpm += hw.encoder.Increment();
-  bpm = max(min(bpm, 300), 20);
-  tickreset = (2.5 * hw.AudioSampleRate()) / bpm;
-  for (int i = 0; i < (int)size; i += 2) {
-    if (ticks >= tickreset)
-      ticks = 0;
-    if (ticks < tickreset / 2 && !hw.button1.Pressed()) {
-      freq = hztofreq(freqhi);
-    } else {
-      freq = hztofreq(freqlo);
-    }
-    ticks++;
+  int bpm = 10 + 16 * (int)(hw.knob1.Value() * 15.9) +
+            (int)(hw.knob2.Value() * 15.9),
+      tickreset = (2.5 * hw.AudioSampleRate()) / bpm;
+  for (int i = 0; i < (int)size; i += 2, ticks++) {
+    ticks = ticks >= tickreset ? 0 : ticks;
+    float freq = ticks < tickreset / 2 && !hw.button1.Pressed()
+                     ? hztofreq(2100)
+                     : hztofreq(1300);
     phase = (phase + freq) - floorf(phase + freq);
     out[i] = out[i + 1] = phase > 0.5 ? 1.0 : -1.0;
   }
 }
 int main(void) {
   hw.Init();
+  hw.StartAdc();
   hw.StartAudio(audio);
   while (1)
     ;
